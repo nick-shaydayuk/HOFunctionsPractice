dab09c093a4d9a4c0ed56943f761fcd9d06a0a01
nick <ghost2394228@gmail.com>
2024-01-29 18:25:14 +0300

    wip

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Практика по объектам
 
-Для работы создайте и используйте ветку develop, это нужно для того, чтобы избежать ошибки при обновлении этого репозитория преподавателем. Если этого не сделать, будет -1 балл :)
 Перед началом работы ознакомьтесь с мэйкфайлом и заданиями.
 Все задания выполняются в index.js и экспортируются не по дефолту.
 
@@ -10,58 +9,68 @@
 
 ### Задание №1
 
-Напишите функцию `isEmpty()`, которая поверхностно проверяет объект на пустоту. Пустыми значениями считаются: '', null, NaN, undefined. **Oblect.keys** в помощь.
+Напишите функцию `convertToLowerOrUpperCase()`, которая на вход принимает массив и вид регистра. Функция должна проходится по массиву и приводит все строки к указанному регистру
 
 **Пример использования**
 
     ```javascript
-    isEmpty({a: true, b: false}); // false
-    isEmpty({a: undefined, b: false}); // false
-    isEmpty({a: undefined}); // true
-    isEmpty({}); // true
+    convertToLowerOrUpperCase(['lower', 'upper'], 'upper'); // ['LOWER', 'UPPER']
+    convertToLowerOrUpperCase(['LOWER', 'UPPER'], 'lower'); // ['lower', 'upper']
     ```
 
 ### Задание №2
 
-Напишите функцию `without()`, которая возвращает новый объект без указанных значений.. Воспользуйтесь **forEach** и деструктуризацией объекта.
+Напишите функцию `convertToFilteredLowerOrUpperCase()`,которая на вход принимает массив и вид регистра. Функция должна проходится по массиву и приводит все строки к указанному регистру.
+Вы спросите, а чем второе задание отличается от первого?
+Ответ: тут вам потребуется добавить фильтрацию так, чтобы возвращаемый массив содержал только строки
 
 **Пример использования**
 
     ```javascript
-    without({ a: 1, b: 2, c: 3 }, 'b', 'c'); // { a: 1 }
-    without({ a: 1, b: 2, c: 3 }, 'a'); // { b: 2, c: 3 }
+    convertToFilteredLowerOrUpperCase(['lower', 'upper'], 'upper'); // ['LOWER', 'UPPER']
+    convertToFilteredLowerOrUpperCase(['LOWER', {a: 123}, 'UPPER'], 'lower'); // ['lower', 'upper']
     ```
 
 ### Задание №3
 
-Напишите функцию `isEmptyDeep()`, которая делает глубокую проверку на пустоту объекта. Пустыми значениями считаются: '', null, NaN, undefined. Воспользуйтесь **Oblect.keys**, оператором **typeof** и **for of** циклом.
+Напишите функцию `filterUsersByAge()`, которая фильтрует пользователей по возрасту. Она должна возвращать массив со всеми совершеннолетними пользователями.
 
 **Пример использования**
 
     ```javascript
-    isEmptyDeep({}); // true
-    isEmptyDeep({ a: { b: undefined } }); // true
-    isEmptyDeep({ a: { b: undefined, c: undefined, d: { e: undefined, f: 1, g: undefined }}}) // false
+    const users = [
+        { name: 'Alice', age: 25 },
+        { name: 'Bob', age: 17 },
+        { name: 'Charlie', age: 30 },
+        { name: 'David', age: 16 },
+    ];
+    filterUsersByAge(users); // [{ name: 'Alice', age: 25 }, { name: 'Charlie', age: 30 }]
     ```
 
 ### Задание №4
 
-Напишите функцию `intersection()`, которая поверхностно находит пересечения объектов и возвращает объект пересечений. Воспользуйтесь встроенной в язык функцией **reduce** у массивов, обратите внимание, что работа с аккумулятором в этот раз будет отличаться от прошлой практики по массивам, вам потребуется узнать, каким образом нужно записывать новую связку ключ-значение в него. А ещё не забывайте про деструктуризацию. Документацию вы найдёте [тут](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce)
+Напишите функцию `filterUsersByParam()`, которая фильтрует пользователей по указанному параметру и значению этого параметра. Она должна возвращать массив со всеми пользователями, прошедшими проверку. Для разных типом значения параметра должны использоваться разные конструкции, а если его тип - число, тогда нужен дополничетельный параметр, который укажет, по какому знаку должна проходить проверка (Допустимые знаки: >, <).
 
 **Пример использования**
 
     ```javascript
-    intersection({ a: 1, b: 2 }, { c: 1, b: 2 }); // { b: 2 }
-    intersection({ a: 1, b: 2, c: 1 }, { c: 1, b: 2 }); // { c: 1, b: 2 }
+    const users = [
+        { name: 'Alice', age: 25, capableOfMarathon: true },
+        { name: 'Bob', age: 17, capableOfMarathon: false },
+        { name: 'Charlie', age: 30, capableOfMarathon: false },
+        { name: 'David', age: 16, capableOfMarathon: true },
+    ];
+    filterUsersByParam(users, 'age', 18, '>'); // [{ name: 'Alice', age: 25 }, { name: 'Charlie', age: 30 }]
+    filterUsersByParam(users, 'capableOfMarathon', true); // [{ name: 'Alice', age: 25 }, { name: 'David', age: 16, capableOfMarathon: true }]
     ```
 
 ### Задание №5
 
-Напишите функцию `intersectionDeep()`, которая глубоко находит пересечения объектов и возвращает объект пересечений. Воспользуйтесь оператором **typeof**, а также рекурсивным вызовом вашей функции.
+Напишите функцию `divisibleAverage()`, которая ищет все элементы в массиве, которые делятся на заданное число и возвращает их среднее арифметическое.
 
 **Пример использования**
 
     ```javascript
-    intersectionDeep({ a: 1, b: { c: 3 } }, { c: 1, b: { c: 3 } }); // { b: { c: 3 } }
-    intersectionDeep({ a: 1, b: { c: 3, d: { e: 5 } } }, { c: 1, b: { c: 3, d: { e: 5 } } }); // { b: { c: 3 }, d: { e: 5 } }
+    divisibleAverage([3, 6, 9, 12, 15]); // 9
+    divisibleAverage([0, -1, 10, 20, 30, 40, 50]); // 15
     ```
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,99 +2,127 @@ import { describe, expect, it } from '@jest/globals';
 import * as funcs from '../index.js';
 
 describe('task1', () => {
-  it('should not be empty', () => {
-    expect(funcs.isEmpty({ a: true, b: false })).toBeFalsy();
+  it('should return undefined', () => {
+    expect(funcs.convertToLowerOrUpperCase()).toBeUndefined();
   });
-  it('should not be empty', () => {
-    expect(funcs.isEmpty({ a: undefined, b: false })).toBeFalsy();
+  it('should work', () => {
+    expect(
+      funcs.convertToLowerOrUpperCase(['lower', 'upper'], 'upper'),
+    ).toEqual(['LOWER', 'UPPER']);
   });
-  it('should be empty', () => {
-    expect(funcs.isEmpty({ a: undefined })).toBeTruthy();
-  });
-  it('should be empty', () => {
-    expect(funcs.isEmpty({})).toBeTruthy();
+  it('should work', () => {
+    expect(
+      funcs.convertToLowerOrUpperCase(['LOWER', 'UPPER'], 'lower'),
+    ).toEqual(['lower', 'upper']);
   });
 });
 
 describe('task2', () => {
-  it('should be without', () => {
-    expect(funcs.without({ a: 1, b: 2, c: 3 }, 'b', 'c')).toEqual({ a: 1 });
-  });
-  it('should be without', () => {
-    expect(funcs.without({ a: 1, b: 2, c: 3 }, 'a')).toEqual({ b: 2, c: 3 });
-  });
   it('should return undefined', () => {
-    expect(funcs.without()).toBeUndefined();
+    expect(funcs.convertToFilteredLowerOrUpperCase()).toBeUndefined();
+  });
+  it('should work', () => {
+    expect(
+      funcs.convertToFilteredLowerOrUpperCase(['lower', 'upper'], 'upper'),
+    ).toEqual(['LOWER', 'UPPER']);
+  });
+  it('should work', () => {
+    expect(
+      funcs.convertToFilteredLowerOrUpperCase(['LOWER', 'UPPER'], 'lower'),
+    ).toEqual(['lower', 'upper']);
+  });
+  it('should work', () => {
+    expect(
+      funcs.convertToFilteredLowerOrUpperCase(
+        ['LOWER', { a: 123 }, 'UPPER'],
+        'lower',
+      ),
+    ).toEqual(['lower', 'upper']);
+  });
+  it('should work', () => {
+    expect(
+      funcs.convertToFilteredLowerOrUpperCase(
+        ['yankee', { a: 123 }, 'go', { b: 456 }, 'home'],
+        'UPPER',
+      ),
+    ).toEqual(['YANKEE', 'GO', 'HOME']);
   });
 });
 
 describe('task3', () => {
-  it('should be thuthy', () => {
-    expect(funcs.isEmptyDeep({ a: { b: undefined } })).toBeTruthy();
+  const users = [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 17 },
+    { name: 'Charlie', age: 30 },
+    { name: 'David', age: 16 },
+  ];
+  const result = [
+    { name: 'Alice', age: 25 },
+    { name: 'Charlie', age: 30 },
+  ];
+  const users2 = [
+    { name: 'Victor', age: 15 },
+    { name: 'Gus', age: 17 },
+    { name: 'Lalo', age: 30 },
+    { name: 'Walter', age: 52 },
+  ];
+  const result2 = [
+    { name: 'Lalo', age: 30 },
+    { name: 'Walter', age: 52 },
+  ];
+  it('should return undefined', () => {
+    expect(funcs.filterUsersByAge()).toBeUndefined();
   });
-  it('should not be truthy', () => {
-    expect(funcs.isEmptyDeep({ a: { b: 1 } })).toBeFalsy();
+  it('should work', () => {
+    expect(funcs.filterUsersByAge(users)).toEqual(result);
   });
-  it('should not be truthy', () => {
-    expect(
-      funcs.isEmptyDeep({
-        a: {
-          b: undefined,
-          c: undefined,
-          d: { e: undefined, f: 1, g: undefined },
-        },
-      }),
-    ).toBeFalsy();
+  it('should work', () => {
+    expect(funcs.filterUsersByAge(users2)).toEqual(result2);
   });
 });
 
 describe('task4', () => {
-  it('should find intersection', () => {
-    expect(funcs.intersection({ a: 1, b: 2 }, { c: 1, b: 2 })).toEqual({
-      b: 2,
-    });
+  const users = [
+    { name: 'Alice', age: 25, capableOfMarathon: true },
+    { name: 'Bob', age: 17, capableOfMarathon: false },
+    { name: 'Charlie', age: 30, capableOfMarathon: false },
+    { name: 'David', age: 16, capableOfMarathon: true },
+  ];
+  const result = [
+    { name: 'Alice', age: 25, capableOfMarathon: true },
+    { name: 'Charlie', age: 30, capableOfMarathon: false },
+  ];
+  const result2 = [
+    { name: 'Alice', age: 25, capableOfMarathon: true },
+    { name: 'David', age: 16, capableOfMarathon: true },
+  ];
+  it('should return undefined', () => {
+    expect(funcs.filterUsersByParam()).toBeUndefined();
   });
-  it('should find intersection', () => {
-    expect(funcs.intersection({ a: 1, b: 2, c: 1 }, { c: 1, b: 2 })).toEqual({
-      c: 1,
-      b: 2,
-    });
+  it('should work', () => {
+    expect(funcs.filterUsersByParam(users, 'age', 18, '>')).toEqual(result);
   });
-  it('should not find intersection', () => {
-    expect(funcs.intersection({ a: 1, b: 2, c: 1 }, { d: 1, e: 2 })).toEqual(
-      {},
-    );
-  });
-  it('should not find intersection', () => {
-    expect(funcs.intersection()).toBeUndefined();
+  it('should work', () => {
+    expect(funcs.filterUsersByParam(users, 'capableOfMarathon', true)).toEqual(result2);
   });
 });
 
 describe('task5', () => {
-  it('should find deep intersection', () => {
-    expect(
-      funcs.intersectionDeep(
-        { a: 1, b: { c: 3, d: { e: 5 } } },
-        { c: 1, b: { c: 3, d: { e: 5 } } },
-      ),
-    ).toEqual({ b: { c: 3, d: { e: 5 } } });
+  it('should count', () => {
+    expect(funcs.divisibleAverage([0, -1, 10, 20, 30, 40, 50], 3)).toEqual(15);
   });
-  it('should find deep intersection', () => {
-    expect(
-      funcs.intersectionDeep({ a: 1, b: { c: 3 } }, { c: 1, b: { c: 3 } }),
-    ).toEqual({
-      b: { c: 3 },
-    });
+  it('should count', () => {
+    expect(funcs.divisibleAverage([0, -1, 10, 20, 30, 40, 50], 2)).toEqual(25);
   });
-  it('should not find deep intersection', () => {
+  it('should count', () => {
     expect(
-      funcs.intersectionDeep(
-        { a: { e: 5 }, b: 2, c: 1 },
-        { d: 1, e: { c: 3 } },
-      ),
-    ).toEqual({});
+      funcs.divisibleAverage([0, -1, 10, 20, 30, 40, 52, 104, 416], 26),
+    ).toEqual(143);
   });
-  it('should not find deep intersection', () => {
-    expect(funcs.intersectionDeep()).toBeUndefined();
+  it('should count', () => {
+    expect(funcs.divisibleAverage([3, 6, 9, 12, 15], 3)).toEqual(9);
+  });
+  it('should not count', () => {
+    expect(funcs.divisibleAverage()).toBeUndefined();
   });
 });
